a032919b24201bb4afd3fb558d9cbcb039c63e44
Pr09rammerali9 <hakamiali197@gmail.com>
2025-06-04 16:48:55 +0300

    Add files via upload

---
+++ ascii.h
@@ -0,0 +1,12 @@
+#ifndef ASCII_H
+#define ASCII_H
+
+/* dont think ur smart, some headers is not finished and might cause problems*/
+#include "src/colors.h"
+#include "src/bg.h"
+#include "src/mode.h"
+#include "src/scr.h"
+#include "src/draw.h"
+#include "src/fonts.h"
+
+#endif
